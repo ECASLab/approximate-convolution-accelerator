9b00e2c60bd41b4b8ab64108d99121b04b257c15
Luis Leon <lleon95@gmail.com>
2021-10-19 20:28:06 -0600

    Fix the matrix print parameters

--- Spatial_convolution/includes/utils/printmatrix.hpp
+++ Spatial_convolution/includes/utils/printmatrix.hpp
@@ -11,22 +11,19 @@
 namespace ama {
 namespace utils {
 
-template <typename T, int M, int N, int k>
+template <typename T, int M, int N>
 
 /**
  * @brief Prints a matrix
  * @tparam T datatype
  * @tparam M the number of rows
  * @tparam N the number of columns
- * @tparam K the size of the kernel
  * @param res a matrix that will be printed in the cmd
  */
 
-void print_matrix(const T res[M + (k - 1)][N + (k - 1)]) {
-  int mk = M + (k - 1);
-  int nk = N + (k - 1);
-  for (int i = 0; i < mk; i++) {
-    for (int j = 0; j < nk; j++) {
+void print_matrix(const T res[M][N]) {
+  for (int i{0}; i < M; ++i {
+    for (int j{0}; j < N; ++j {
       std::cout << res[i][j] << ' ';
     }
     std::cout << std::endl;
